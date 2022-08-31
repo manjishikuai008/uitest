dc5cac9b23eff971637b1fa8c0636187bbc828e3
慢即是快 <1443974396@qq.com>
2022-08-31 18:48:31 +0800

    自动化+性能

--- scripts/test01_mp.py
+++ scripts/test01_mp.py
@@ -1,24 +1,34 @@
+import sys
+import os
+sys.path.append(os.getcwd())
 import pytest
+from locust import TaskSet, task, HttpLocust
 
 from api.api_mp import ApiMp
 import api
 from tool.get_log import GetLog
 from tool.read_yaml import read_yaml
 from tool.tool import Tool
+
 log = GetLog.get_logger()
 
 
-class TestMp:
+class TestMp(TaskSet):
     # 1. 初始化
     def setup_class(self):
         # 获取ApiMp对象
         self.mp = ApiMp()
 
+    # locust中初始化方法
+    def on_start(self):
+        self.setup_class()
+
     # 2. 登录接口测试方法
-    @pytest.mark.parametrize("mobile,code", read_yaml("mp_login.yaml"))
-    def test01_mp_login(self, mobile, code):
+    # @pytest.mark.parametrize("mobile,code", read_yaml("mp_login.yaml"))
+    @task
+    def test01_mp_login(self, mobile="13812345678", code="246810"):
         # 调用登录接口
-        r = self.mp.api_mp_login(mobile, code)
+        r = self.mp.api_mp_login(self, mobile, code)
         # 打印输出结果
         print("登录的结果为：", r.json())
         try:
@@ -42,9 +52,10 @@ class TestMp:
         # assert "OK" == r.json().get("message")
 
     # 3. 发布文章测试接口方法
+    @task
     def test02_mp_article(self, title=api.title, content=api.content, channel_id=api.channel_id):
         # 1. 调用发布文章接口
-        r = self.mp.api_mp_article(title, content, channel_id)
+        r = self.mp.api_mp_article(self, title, content, channel_id)
         # 2. 提取id
         api.article_id = r.json().get("data").get("id")
         print("发布文章成功后的id值为：", api.article_id)
@@ -56,3 +67,11 @@ class TestMp:
             log.error(e)
             # 2. 抛异常
             raise
+
+
+# 用户类
+class UserRun(HttpLocust):
+    # 1. task_set
+    task_set = TestMp
+    # 2. host
+    host = api.host
